137ee0291c41eab23a618f122f6e357cf88f8f00
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-05-03 23:13:36 +0200

    Complete tests for UserResource

--- src/test/java/io/github/pascalgrimaud/qualitoast/web/rest/UserResourceIntTest.java
+++ src/test/java/io/github/pascalgrimaud/qualitoast/web/rest/UserResourceIntTest.java
@@ -527,12 +527,20 @@ public class UserResourceIntTest {
     @Transactional
     public void getAllAuthorities() throws Exception {
         restUserMockMvc.perform(get("/api/users/authorities")
-                .accept(TestUtil.APPLICATION_JSON_UTF8)
-                .contentType(TestUtil.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$").value(containsInAnyOrder("ROLE_USER", "ROLE_ADMIN")));
+            .accept(TestUtil.APPLICATION_JSON_UTF8)
+            .contentType(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$").value(containsInAnyOrder("ROLE_USER", "ROLE_ADMIN")));
+    }
+
+    @Test
+    public void search() throws Exception {
+        restUserMockMvc.perform(get("/api/_search/users/*")
+            .accept(TestUtil.APPLICATION_JSON_UTF8)
+            .contentType(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
     }
 
     @Test
