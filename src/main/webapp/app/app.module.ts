cc84ae7cbe1b1658aebeb2e9e1f1da9fa1b8eb87
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-05-21 10:13:13 +0200

    Add primeng dependencies and Dashboard page

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -2,6 +2,7 @@ import './vendor.ts';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Ng2Webstorage } from 'ng2-webstorage';
 
 import { QualiToastSharedModule, UserRouteAccessService } from './shared';
@@ -9,6 +10,7 @@ import { QualiToastHomeModule } from './home/home.module';
 import { QualiToastAdminModule } from './admin/admin.module';
 import { QualiToastAccountModule } from './account/account.module';
 import { QualiToastEntityModule } from './entities/entity.module';
+import { QualiToastDashboardModule } from './dashboard/dashboard.module';
 
 import { customHttpProvider } from './blocks/interceptor/http.provider';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
@@ -33,7 +35,9 @@ import {
         QualiToastHomeModule,
         QualiToastAdminModule,
         QualiToastAccountModule,
-        QualiToastEntityModule
+        QualiToastEntityModule,
+        QualiToastDashboardModule,
+        BrowserAnimationsModule
     ],
     declarations: [
         JhiMainComponent,
