ed55b8bff43d9765bd0d4d92df66bda0f53be155
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-05-26 23:44:59 +0200

    Add alert when launching elasticsearch reindex

--- src/main/webapp/app/admin/elasticsearch-reindex/elasticsearch-reindex-modal.component.ts
+++ src/main/webapp/app/admin/elasticsearch-reindex/elasticsearch-reindex-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AlertService } from 'ng-jhipster';
 
 import { ElasticsearchReindexService } from './elasticsearch-reindex.service';
 
@@ -11,10 +12,13 @@ export class ElasticsearchReindexModalComponent {
 
     constructor(
         private elasticsearchReindexService: ElasticsearchReindexService,
-        public activeModal: NgbActiveModal) { }
+        public activeModal: NgbActiveModal,
+        private alertService: AlertService
+    ) { }
 
     reindex() {
         this.elasticsearchReindexService.reindex().subscribe();
         this.activeModal.dismiss(true);
+        this.alertService.info('elasticsearch.reindex.accepted', null, null);
     }
 }
